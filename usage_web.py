9651b2e1f5a31fc7983f92fd7c6e08ddd0c3b57c
FaezeShakuri <f.shakori.com@gmail.com>
2021-10-28 13:53:37 +0330

    Modified main function to get traceback and return answer

--- usage_web.py
+++ usage_web.py
@@ -3,12 +3,7 @@ from pycee.errors import handle_error
 from pycee.utils import parse_args_by_file_name, remove_cache, return_answers
 from pycee.inspection import get_error_info_from_traceback
 
-def main():
-
-    traceback = """Traceback (most recent call last):
-  File "example_code.py", line 2, in <module>
-    print(arr[0])
-IndexError: list index out of range"""
+def main(traceback):
 
     error_info = get_error_info_from_traceback(traceback)
 
@@ -20,7 +15,16 @@ IndexError: list index out of range"""
     query, pycee_hint, pydoc_answer = handle_error(error_info, args)
     so_answers, _ = get_answers(query, error_info, args)
     solution = return_answers(so_answers, pycee_hint, pydoc_answer, args)
-    print(solution)
 
-if __name__ == "__main__":
-    main()
+    return solution
+
+
+# Uncomment to test main function
+# if __name__ == "__main__":
+#
+#     _traceback = """Traceback (most recent call last):
+#   File "example_code.py", line 2, in <module>
+#     print(arr[0])
+# IndexError: list index out of range"""
+#
+#     print(main(_traceback))
