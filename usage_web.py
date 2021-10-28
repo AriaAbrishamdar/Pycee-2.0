64db7a5e71a23da014cd51fb6d4c20ef8abfb620
FaezeShakuri <f.shakori.com@gmail.com>
2021-10-28 12:44:12 +0330

    Add usage_web.py to get traceback as a string and print the solutions

---
+++ usage_web.py
@@ -0,0 +1,26 @@
+from pycee.answers import get_answers
+from pycee.errors import handle_error
+from pycee.utils import parse_args_by_file_name, remove_cache, print_answers
+from pycee.inspection import get_error_info_from_traceback
+from pprint import pprint
+
+def main():
+
+    traceback = """Traceback (most recent call last):
+  File "example_code.py", line 2, in <module>
+    print(arr[0])
+IndexError: list index out of range"""
+
+    error_info = get_error_info_from_traceback(traceback)
+
+    args = parse_args_by_file_name([error_info['file']])
+
+    if args.rm_cache:
+        remove_cache()
+
+    query, pycee_hint, pydoc_answer = handle_error(error_info, args)
+    so_answers, _ = get_answers(query, error_info, args)
+    print_answers(so_answers, pycee_hint, pydoc_answer, args)
+
+if __name__ == "__main__":
+    main()
