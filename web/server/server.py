a9e228bcc435fe47fba068696a4f8bbfbef7d144
Aria <32393394+AriaAbrishamdar@users.noreply.github.com>
2021-11-01 17:24:15 +0330

    Updated server.py

--- web/server/server.py
+++ web/server/server.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+import usage_web
+
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     """Request in server"""
-    text = str(len(str(request.json["text"])))
-    return text
+    traceback = str(request.json["error"])
+    code = str(request.json["code"])
+    result = usage_web.main(_traceback=traceback, _code=code, _colored=False)
+    return result
 
 if __name__ == '__main__':
     app.run()
