7664a63a9aa67dce74b5788cc87761941db3fb6c
Amirkalateh <amirkalateh@outlook.com>
2021-12-04 12:55:17 +0330

    Update application_web.py

--- application_web.py
+++ application_web.py
@@ -25,7 +25,37 @@ def create_JSON(so_answers: list, links: list):
 
     return json.dumps(data_set)
 
+def write_json(new_data, filename='updown.json'):
+    with open(filename,'r+') as file:
+        file_data = json.load(file)
+        flag = 0
+        for x in file_data['data']:
+            if new_data['solution_link'] in x['solution_link'] and new_data['error_type'] in x['error_type'] :
+                print("true")
+                flag = 1
+                if new_data['value'] == 1 :
+                    x['value'] = x['value'] + 1
+                if new_data['value'] == -1 :
+                    x['value'] = x['value'] - 1   
+                with open(filename,'w') as file: 
+                    json.dump(file_data,file,indent = 4)
+                break
 
+        if flag != 1 :    
+            file_data["data"].append(new_data)
+            file.seek(0)
+            json.dump(file_data, file, indent = 4) 
+
+def updownvote(solution_link : str ,error_type : str , value : int):
+    
+    data = {
+        "solution_link":solution_link,
+        "error_type":error_type,
+        "value":value
+    }
+    return data
+
+     
 def main(_traceback: str, _code: str, _n_answers=0, _colored=False, _json=True):
 
     # Get error information
