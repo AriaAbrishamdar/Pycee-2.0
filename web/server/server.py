2f96bdbdaafa71527afaaee1bdcdf08570cb0939
Aria <32393394+AriaAbrishamdar@users.noreply.github.com>
2021-12-17 11:49:37 +0330

    Update server.py

--- web/server/server.py
+++ web/server/server.py
@@ -10,23 +10,35 @@ CORS(app)
 @app.route("/", methods=['GET', 'POST'])
 def home():
 
-    type = str(request.json["type"])
+    msg = request.json
 
-    """find solutions"""
-    if (type == "find_solutions"):
-        traceback = str(request.json["error"])
-        code = str(request.json["code"])
-        number_of_solutions = str(request.json["number_of_solutions"])
-        result = application_web.main(_traceback=traceback, _code=code, _colored=False,  _n_answers=number_of_solutions)
-        return result
+    if (msg == None):
+        return { "successful": [0] }
 
-    """upvote - downvote"""
-    if ((type == "upvote") or (type == "downvote")):
-        error_type = str(request.json["error_type"])
-        link = str(request.json["link"])
-        value = request.json["value"]
-        application_web.send_updownvote(link, error_type, value);
-        return  { "successful": [value] }
+    elif ("type" not in msg):
+        return { "successful": [0] }
+
+    else:
+        request_type = str(msg["type"])
+
+        """find solutions"""
+        if (request_type == "find_solutions"):
+            traceback = str(msg["error"])
+            code = str(msg["code"])
+            number_of_solutions = str(msg["number_of_solutions"])
+            result = application_web.main(_traceback=traceback, _code=code, _colored=False,  _n_answers=number_of_solutions)
+            return result
+
+        """upvote - downvote"""
+        if ((request_type == "upvote") or (request_type == "downvote")):
+            error_type = str(msg["error_type"])
+            link = str(msg["link"])
+            value = msg["value"]
+            application_web.send_updownvote(link, error_type, value);
+            return  { "successful": [1] }
+
+        else:
+            return { "successful": [0] }
 
 
 if __name__ == '__main__':
