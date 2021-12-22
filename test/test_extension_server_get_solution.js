da357c1b97f14a3fc9bc761c6b4b8e72f58b53f3
Aria <32393394+AriaAbrishamdar@users.noreply.github.com>
2021-12-22 16:18:56 +0330

    Update test_extension_server_get_solution.js

--- test/test_extension_server_get_solution.js
+++ test/test_extension_server_get_solution.js
@@ -1,15 +1,16 @@
-function test(error, code) {
+function test_get_solutions(error, code) {
 
     var request = new XMLHttpRequest();
 
     request.onreadystatechange = function() {
         if (request.readyState === 4) {
-            console.log(request.response);
+            console.log(request.response.items.length);
         }
     }
+    var number_of_solutions = 3;
     var type = "find_solutions";
     request.open("POST", 'https://ariaabrishamdar.pythonanywhere.com/', true);
-    var msg = { type, error, code };
+    var msg = { type, error, code, number_of_solutions };
     var msgjson = JSON.stringify(msg);
     request.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
     request.send(msgjson);
@@ -17,6 +18,6 @@ function test(error, code) {
 
 
 
-test("File "main.py", line 2\n          ^\nSyntaxError: unexpected EOF while parsing", "print(");
-test("Traceback (most recent call last):\n  File "main.py", line 2, in <module>\n    print(arr[0])\nIndexError: list index out of range", "arr = []\nprint(arr[0])");
-test("  File "main.py", line 1\n    print(1)\n    ^\nIndentationError: unexpected indent", "    print(1)");
+test_get_solutions("File "main.py", line 2\n          ^\nSyntaxError: unexpected EOF while parsing", "print(");
+test_get_solutions("Traceback (most recent call last):\n  File "main.py", line 2, in <module>\n    print(arr[0])\nIndexError: list index out of range", "arr = []\nprint(arr[0])");
+test_get_solutions("File "main.py", line 1\n    print(1)\n    ^\nIndentationError: unexpected indent", "    print(1)");
